28b0a28af26629580ceb62ee5c5da3f05944e5aa
sangria-sangre <114749225+sangria-sangre@users.noreply.github.com>
2023-06-12 00:46:50 +0300

    done

--- models/card.js
+++ models/card.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-import validator from 'validator';
+const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
   name: {
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-import validator from 'validator';
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
